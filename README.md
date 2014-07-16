62a392e5ee769cf96d9d3d80faaf83ca8b05f617
Christian Telles <chrisempire152@hotmail.com>
2014-07-16 18:45:50 -0500

    Updated timing and click counter functions for each delay setting. Organized code.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Displays readings of Accelerometer and Gyroscope sensors in different Sensor Delay frequencies, and displays the amount of time elapsed using that specific delay setting.
+
+Note to self: total seconds does not update correctly.
--- src/com/example/gyroaccelapp/MainActivity.java
+++ src/com/example/gyroaccelapp/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.gyroaccelapp;
 
-import com.example.gyroaccelapp.R.layout;
-
 import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -11,6 +9,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -27,53 +27,69 @@ public class MainActivity extends Activity implements SensorEventListener {
 	// Defines variables and sensors before the class they are used in
 
 	final String tag = "GAP";
+
 	SensorManager mSensorManager = null;
-	TextView xViewA = null;
-	TextView yViewA = null;
-	TextView zViewA = null;
-	TextView xViewG = null;
-	TextView yViewG = null;
-	TextView zViewG = null;
+
+	TextView mxViewA = null;
+	TextView myViewA = null;
+	TextView mzViewA = null;
+	TextView mxViewG = null;
+	TextView myViewG = null;
+	TextView mzViewG = null;
+	TextView mNormTime;
+	TextView mGameTime;
+	TextView mUITime;
+	TextView mFastTime;
+	TextView mAllTime;
+
+	String mTextDelay;
+
 	Sensor mAccel;
 	Sensor mGyro;
+
+	long duration;
+	int dur = (int) duration;
+
+	long startTime;
+	long endTime;
+
+	long mEndT;
+	long mEndN;
+	long mEndG;
+	long mEndU;
+	long mEndF;
+
 	int mNorm;
 	int mGame;
 	int mUI;
 	int mFast;
 	int mDelay;
-	float axisX;
-	float axisY;
-	float axisZ;
-	float norm;
-	float game;
-	float ui;
-	long elapsedMillis;
-	Chronometer fast;
-	TextView normalTime;
-	TextView nTime;
-	TextView gTime;
-	TextView uTime;
-	TextView fTime;
-	TextView allTime;
-	String delay;
+
+	int prevNTime = 1;
+	int prevUTime = 2;
+	int prevGTime = 3;
+	int prevFTime = 4;
+	int prevTime;
+
+	int modeN = 1;
+	int modeU = 2;
+	int modeG = 3;
+	int modeF = 4;
+	int mode;
+
+	int mClickN;
+	int mClickU;
+	int mClickG;
+	int mClickF;
+	int mClick;
+
+	float mAxisX;
+	float mAxisY;
+	float mAxisZ;
+
 	Chronometer focus;
+
 	Button normD, gameD, uiD, fastD;
-	TextView mText;
-	String minute;
-	String hour;
-	String time;
-	long stoppedMilliseconds = 0;
-	TextView mon;
-	long clock;
-	long elapsedTime;
-	long timeClock;
-	TextView elapsation;
-	int clockization = (int) timeClock;
-	int aclock = (int) clock;
-	long duration;
-	int dur = (int) duration;
-	long startTime;
-	long endTime;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -81,7 +97,6 @@ public class MainActivity extends Activity implements SensorEventListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-		// final RelativeLayout relativeLayout = new RelativeLayout(this);
 
 		mAccel = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		mGyro = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
@@ -91,88 +106,74 @@ public class MainActivity extends Activity implements SensorEventListener {
 		mUI = SensorManager.SENSOR_DELAY_UI;
 		mFast = SensorManager.SENSOR_DELAY_FASTEST;
 
+		mDelay = mNorm;
 		mSensorManager.registerListener(this, mAccel, mDelay);
 		mSensorManager.registerListener(this, mGyro, mDelay);
 
-		xViewA = (TextView) findViewById(R.id.xbox);
-		yViewA = (TextView) findViewById(R.id.ybox);
-		zViewA = (TextView) findViewById(R.id.zbox);
-		xViewG = (TextView) findViewById(R.id.xboxo);
-		yViewG = (TextView) findViewById(R.id.yboxo);
-		zViewG = (TextView) findViewById(R.id.zboxo);
+		mxViewA = (TextView) findViewById(R.id.xbox);
+		myViewA = (TextView) findViewById(R.id.ybox);
+		mzViewA = (TextView) findViewById(R.id.zbox);
+		mxViewG = (TextView) findViewById(R.id.xboxo);
+		myViewG = (TextView) findViewById(R.id.yboxo);
+		mzViewG = (TextView) findViewById(R.id.zboxo);
 
-		nTime = (TextView) findViewById(R.id.chronoNormal);
-		gTime = (TextView) findViewById(R.id.chronoGame);
-		uTime = (TextView) findViewById(R.id.chronoUI);
-		fTime = (TextView) findViewById(R.id.chronoFast);
+		mNormTime = (TextView) findViewById(R.id.chronoNormal);
+		mGameTime = (TextView) findViewById(R.id.chronoGame);
+		mUITime = (TextView) findViewById(R.id.chronoUI);
+		mFastTime = (TextView) findViewById(R.id.chronoFast);
 
 		normD = (Button) findViewById(R.id.ND);
 		gameD = (Button) findViewById(R.id.GD);
 		uiD = (Button) findViewById(R.id.UD);
 		fastD = (Button) findViewById(R.id.FD);
 
-		timeClock = SystemClock.uptimeMillis();
-		clock = SystemClock.elapsedRealtime();
-		focus = (Chronometer) findViewById(R.id.chronometer1);
-
-		elapsation = (TextView) findViewById(R.id.timeElapsed);
-
 		startTime = System.nanoTime();
+		focus = (Chronometer) findViewById(R.id.chronometer1);
+		focus.setBase(SystemClock.elapsedRealtime());
+		focus.start();
 
-		// long elapsedMillis = SystemClock.elapsedRealtime() - focus.getBase();
-		// focus = new Chronometer(this);
-		// relativeLayout.addView(focus);
-		// setContentView(relativeLayout);
+		// GridView gridview = (GridView) findViewById(R.id.gridview);
+		// gridview.setAdapter(new ImageAdapter(this));
 		//
-		// int size = 1;
-		// TextView[] tv = new TextView[size];
-		// for (int n = 0; n < size; n++) {
-		// nTime = new TextView(this);
-		// relativeLayout.addView(nTime);
-		// tv[n] = nTime;
+		// gridview.setOnItemClickListener(new OnItemClickListener() {
+		// public void onItemClick(AdapterView<?> parent, View v, int position,
+		// long id) {
+		// Toast.makeText(HelloGridView.this, "" + position,
+		// Toast.LENGTH_SHORT).show();
 		// }
+		// });
+
+		// create a separate start time and end time for each delay setting
+
 		normD.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-
-				endTime = System.nanoTime();
-				mDelay = mNorm;
-				allTime = nTime;
-				nTime.setText(Integer.toString(dur));
-				delay();
-			}
-		});
-
-		gameD.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				endTime = System.nanoTime();
-				mDelay = mGame;
-				allTime = gTime;
-				gTime.setText(Integer.toString(dur));
-				delay();
+				prevTime = prevNTime;
+				previousTime();
 			}
 		});
 
 		uiD.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				endTime = System.nanoTime();
-				mDelay = mUI;
-				allTime = uTime;
-				uTime.setText(Integer.toString(dur));
-				delay();
+				prevTime = prevUTime;
+				previousTime();
+			}
+		});
+
+		gameD.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				prevTime = prevGTime;
+				previousTime();
 			}
 		});
 
 		fastD.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				endTime = System.nanoTime();
-				mDelay = mFast;
-				allTime = fTime;
-				fTime.setText(Integer.toString(dur));
-				delay();
+				prevTime = prevFTime;
+				previousTime();
 			}
 		});
 
@@ -183,19 +184,19 @@ public class MainActivity extends Activity implements SensorEventListener {
 				+ event.values[0] + ", y: " + event.values[1] + ", z: "
 				+ event.values[2]);
 
-		axisX = event.values[0];
-		axisY = event.values[1];
-		axisZ = event.values[2];
+		mAxisX = event.values[0];
+		mAxisY = event.values[1];
+		mAxisZ = event.values[2];
 
 		if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-			xViewG.setText("Gyro X: " + axisX);
-			yViewG.setText("Gyro Y: " + axisY);
-			zViewG.setText("Gyro Z: " + axisZ);
+			mxViewG.setText("Gyro X: " + mAxisX);
+			myViewG.setText("Gyro Y: " + mAxisY);
+			mzViewG.setText("Gyro Z: " + mAxisZ);
 		}
 		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-			xViewA.setText("Accel X: " + axisX);
-			yViewA.setText("Accel Y: " + axisY);
-			zViewA.setText("Accel Z: " + axisZ);
+			mxViewA.setText("Accel X: " + mAxisX);
+			myViewA.setText("Accel Y: " + mAxisY);
+			mzViewA.setText("Accel Z: " + mAxisZ);
 		}
 
 	}
@@ -216,65 +217,110 @@ public class MainActivity extends Activity implements SensorEventListener {
 		Log.d(tag, "onAccuracyChanged: " + sensor + ", accuracy: " + accuracy);
 	}
 
+	public void previousTime() {
+
+		endTime = System.nanoTime();
+
+		mode();// Calculates time since last button click
+
+		if (prevTime == prevNTime) {
+
+			mClickN++; // Increases click counter
+			mClick = mClickN; // Sets displayed counter to current click counter
+
+			mDelay = mNorm; // Sensor Delay Setting
+			mAllTime = mNormTime; // TextView
+			mTextDelay = "Normal"; // String for TextView
+
+			mode = modeN;
+
+			mNormTime.setText(String.valueOf(duration)); // Converts long to
+															// string
+
+		} else if (prevTime == prevUTime) {
+
+			mClickU++;
+			mClick = mClickU;
+
+			mDelay = mUI;
+			mAllTime = mUITime;
+			mTextDelay = "UI";
+
+			mode = modeU;
+
+			mUITime.setText(String.valueOf(duration));
+
+		} else if (prevTime == prevGTime) {
+
+			mClickG++;
+			mClick = mClickG;
+
+			mDelay = mGame;
+			mAllTime = mGameTime;
+			mTextDelay = "Game";
+
+			mode = modeG;
+
+			mGameTime.setText(String.valueOf(duration));
+
+		} else if (prevTime == prevFTime) {
+
+			mClickF++;
+			mClick = mClickF;
+
+			mDelay = mFast;
+			mAllTime = mFastTime;
+			mTextDelay = "Fast";
+
+			mode = modeF;
+
+			mFastTime.setText(String.valueOf(duration));
+
+		}
+
+		delay(); // Starts function which registers listener and sets text
+
+	}
+
 	public void delay() {
 		mSensorManager.unregisterListener(this);
 		mSensorManager.registerListener(this, mAccel, mDelay);
 		mSensorManager.registerListener(this, mGyro, mDelay);
-		focus.setBase(SystemClock.elapsedRealtime());
-		focus.start();
-		elapsed();
-		allTime.setText(delay + " Delay Elapsed: " + time);
-		// uptimeMillis.start();
-		duration = endTime - startTime;
-		elapsation.setText(Integer.toString(dur));
+		endTime = System.nanoTime();
 		startTime = System.nanoTime();
+		mAllTime.setText(mTextDelay + " in milli: " + duration
+				+ " | total secs:" + mEndT + " | clicks: " + mClick);
 
-		// try {
-		// hour = String.valueOf(mText.getText());
-		// minute = String.valueOf(stoppedMilliseconds - focus.getBase());
-		// int t = java.lang.Integer.parseInt(minute) / 1000 / 60;
-		// allTime.setText(Integer.toString(t));
-		// time = "" + t;
-		// } finally {
-		// Toast.makeText(this, "Elapsing " + delay + " Delay Time",
-		// Toast.LENGTH_SHORT).show();
-		// }
 	}
 
-	public void elapsed() {
-		if (allTime == nTime) {
-			delay = "Normal";
-		} else if (allTime == gTime) {
-			delay = "Game";
-		} else if (allTime == uTime) {
-			delay = "UI";
-		} else if (allTime == fTime) {
-			delay = "Fast";
+	public void mode() {
+
+		duration = (endTime - startTime) / 1000000; // Gets last click time and
+													// converts to milliseconds
+
+		if (mode == modeN) {
+
+			mEndN = mEndN + duration; // Adds time since last to total time of
+										// button being clicked
+			mEndT = mEndN / 1000;// Converts milliseconds to seconds
+
+		} else if (mode == modeU) {
+
+			mEndU = mEndU + duration;
+			mEndT = mEndU / 1000;
+
+		} else if (mode == modeG) {
+
+			mEndG = mEndG + duration;
+			mEndT = mEndG / 1000;
+
+		} else if (mode == modeF) {
+
+			mEndF = mEndF + duration;
+			mEndT = mEndF / 1000;
+
 		}
+
 	}
 
-	// public void showElapsedTime() {
-	// elapsedMillis = SystemClock.elapsedRealtime() - focus.getBase();
-	// normalTime.makeText(
-	// "Elapsed milliseconds: "
-	// + Integer.toString((int) elapsedMillis)).show();
-	// normalTime = ((TextView) nTime);
-	// }
-	//
-	// public void onClick(View v) {
-	//
-	// String chronoText = focus.getText().toString();
-	// String array[] = chronoText.split(":");
-	// if (array.length == 2) {
-	// stoppedMilliseconds = Integer.parseInt(array[0]) * 60 * 1000
-	// + Integer.parseInt(array[1]) * 1000;
-	// } else if (array.length == 3) {
-	// stoppedMilliseconds = Integer.parseInt(array[0]) * 60 * 60 * 1000
-	// + Integer.parseInt(array[1]) * 60 * 1000
-	// + Integer.parseInt(array[2]) * 1000;
-	// }
-	//
-	// focus.setBase(SystemClock.elapsedRealtime() - stoppedMilliseconds);
-	// focus.start();
-	// }
 }
