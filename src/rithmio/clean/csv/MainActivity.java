508c25e35cb7361eb6a9eaf69329b177397488d2
Christian Telles <chrisempire152@hotmail.com>
2014-08-05 11:30:13 -0500

    Changed some names

--- src/rithmio/clean/csv/MainActivity.java
+++ src/rithmio/clean/csv/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.gyroaccelapp.clean;
+package rithmio.clean.csv;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
--- src/rithmio/clean/csv/MySensorListener.java
+++ src/rithmio/clean/csv/MySensorListener.java
@@ -1,4 +1,4 @@
-package com.example.gyroaccelapp.clean;
+package rithmio.clean.csv;
 
 import java.io.File;
 import java.io.FileNotFoundException;
